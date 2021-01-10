c6be2d1bbb140e77dae2ea3a66a62ab4f991bf6d
GitHub Action <action@github.com>
2021-01-10 14:23:16 +0000

    [chore] Auto publish

--- zh-tw.search.min.222ad4cd71dd098700b7ec4a9b5c2361d355ce2e4a91fdf9c4f6d0d111682868.js
+++ zh-tw.search.min.222ad4cd71dd098700b7ec4a9b5c2361d355ce2e4a91fdf9c4f6d0d111682868.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/zh-tw.search-data.min.c5f67fd2a7561bc19a10dd74ad1dbb4543633d37b77f254dbb9155082354bf34.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/zh-tw.search-data.min.413424db2de5faf49d5f8df83aae2a8982de441f083cb3c3ce41ba51507136c0.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
